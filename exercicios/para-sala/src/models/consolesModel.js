8c738157f1596cf7d168e89e4171b86fd1a2e275
pâmela santos <pssantos201@gmail.com>
2022-11-08 13:52:58 -0300

    :sparkles: feat: add consoles model with mongoose schema

---
+++ exercicios/para-sala/src/models/consolesModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+const consoleSchema = new mongoose.Schema(
+
+    {
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            default: mongoose.Types.ObjectId
+        },
+        name: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        developer: {
+            type: String,
+            required: true
+        },
+        releaseDate: {
+            type: Number,
+            required: true
+        },
+        display: {
+            type: [String],
+            required: true
+        },
+        storageCapacities: {
+            type: [String],
+            required: true
+        },
+        numberOfPlayers: {
+            type: [Number],
+            required: true
+        },
+        available: {
+            type: Boolean,
+            required: true
+        },
+        description: {
+            type: [String],
+            minLength: 0,
+            maxLength: 1000,
+            default: "no description"
+        },
+        
+    },
+    {
+        timeStamp: true
+    }
+)
+
+const Model = mongoose.model("console", consoleSchema)
+
+module.exports = Model
