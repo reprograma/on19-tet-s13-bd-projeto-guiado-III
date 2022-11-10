9f88512a1cef72c6269f1a41886ee5d696da33e2
pâmela santos <pssantos201@gmail.com>
2022-11-10 17:45:51 -0300

    :sparkles: feat: add games routes

--- exercicios/para-sala/src/routes/gamesRoute.js
+++ exercicios/para-sala/src/routes/gamesRoute.js
@@ -4,9 +4,11 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get('/all', controller.findAllGames)
-router.get("/:id", controller.findGameById)
-router.post("/add", controller.addNewGame)
+router.get('/all', controller.findAllGames);
+router.get("/search/:gamebygenre", controller.findByGenre);
+router.get("/:name", controller.findGameByName);
+router.get("/:id", controller.findGameById);
+router.post("/add", controller.addNewGame);
 router.patch("/:id", controller.updateGame); 
 router.delete("/:id", controller.deleteGame); 
 
