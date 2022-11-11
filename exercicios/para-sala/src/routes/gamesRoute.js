c1edf496abd6fe30d278512a4992b7e8db0c26cf
Djeisly Rodrigues <djeisly@yahoo.com.br>
2022-11-11 13:18:10 -0300

    Add correção de linha comentada

--- exercicios/para-sala/src/routes/gamesRoute.js
+++ exercicios/para-sala/src/routes/gamesRoute.js
@@ -6,10 +6,8 @@ const router = express.Router();
 router.get("/all", controller.findAllGames);
 router.get("/name", controller.findGameByName);
 router.post("/add", controller.addNewGame);
-
 router.get("/:id", controller.findGameById);
 router.patch("/:id", controller.updateGame);
-
 router.delete("/:id", controller.deleteGame);
 
 module.exports = router;
