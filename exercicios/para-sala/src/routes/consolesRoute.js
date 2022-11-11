e70bc6ec40c01a5e68e458f87f6d763768457dd6
Djeisly Rodrigues <djeisly@yahoo.com.br>
2022-11-11 13:26:23 -0300

    Add Update

--- exercicios/para-sala/src/routes/consolesRoute.js
+++ exercicios/para-sala/src/routes/consolesRoute.js
@@ -4,20 +4,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/all", controller.findAllConsoles);
-
-router.get("/:id", controller.findConsoleById);
-
-
-
-
-router.get("/:id", controller.findConsoleByName);
-
-
-
 router.post("/add", controller.addNewConsole);
-
+router.get("/name", controller.findConsoleByName);
+router.get("/:id", controller.findConsoleById);
 router.patch("/:id", controller.updateConsole);
-
 router.delete("/:id", controller.deleteConsole);
 
 module.exports = router;
